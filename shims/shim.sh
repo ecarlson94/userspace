1948889bc8b6d0a48e1f18fdbdf08047db389663
Eric Carlson <e.carlson94@gmail.com>
2021-04-13 23:55:30 +0000

    finish python install, link python3 to python

---
+++ shims/shim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo docker run -it --rm \
+  --privileged \
+  -v $HOME/Documents:/Documents:rw \
+  -w $HOME \
+  -v $HOME/.dotfiles:/dotfiles:rw \
+  -v $PWD:/cwd:rw \
+  -v $HOME/.config:/config:rw \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v ${HOME}/.gnupg/:/.gnupg/:ro \
+  -v /run/user/$(id -u)/:/run/user/$(id -u)/:ro \
+  -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK \
+  -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+  -p 1234:1234 \
+  -p 3000:3000 \
+  -p 5000:5000 \
+  -u `id -u` \
+  walawren/dev-container:latest "$@"
