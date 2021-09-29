530e345fc45e910265e82bc29dc97507e649807a
Eric Carlson <e.carlson94@gmail.com>
2021-09-29 03:19:36 +0000

    create tmux session folder before docker command

--- shims/shim.sh
+++ shims/shim.sh
@@ -10,6 +10,10 @@ gpgconf --launch gpg-agent
 # Identity Card unlock
 ssh git@github.com
 
+# Create tmxu session directory
+tmuxDir=$HOME/.tmux/resurrect
+mkdir -p $tmuxDir
+
 # Userspace Command
 docker run -it --rm \
   --privileged \
@@ -17,7 +21,7 @@ docker run -it --rm \
   -w $HOME \
   -v $PWD:/cwd:rw \
   -v $HOME/.config:/config:rw \
-  -v $HOME/.tmux/resurrect:/home/walawren/.tmux/resurrect:rw \
+  -v $tmuxDir:/home/walawren/.tmux/resurrect:rw \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v ${HOME}/.gnupg/:/.gnupg/:ro \
   -v /run/user/$(id -u)/:/run/user/$(id -u)/:ro \
