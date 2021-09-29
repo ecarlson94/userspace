04c63b0e69e8831af77b0e32d5a3f3604b6efd4f
Eric Carlson <e.carlson94@gmail.com>
2021-09-29 00:42:21 +0000

    add identity card unlock to shim

--- shims/shim.sh
+++ shims/shim.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# SSH Passthrough Setup
 eval $(which ssh-agent)
 export TTY=$(tty)
 export GPG_TTY=$TTY
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 echo UPDATESTARTUPTTY | gpg-connect-agent > /dev/null
 gpgconf --launch gpg-agent
+# Identity Card unlock
+ssh git@github.com
 
+# Userspace Command
 docker run -it --rm \
   --privileged \
   -v $HOME/Documents:/Documents:rw \
   -w $HOME \
-  -v $HOME/.dotfiles:/dotfiles:rw \
   -v $PWD:/cwd:rw \
   -v $HOME/.config:/config:rw \
+  # Docker sock passthrough
   -v /var/run/docker.sock:/var/run/docker.sock \
+  # SSH Configuration
   -v ${HOME}/.gnupg/:/.gnupg/:ro \
   -v /run/user/$(id -u)/:/run/user/$(id -u)/:ro \
   -v $SSH_AUTH_SOCK:$SSH_AUTH_SOCK \
